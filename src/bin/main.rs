2bab5b98f62ccdf5f13b23651602e49909340b7b
dhruv-chawla <dhruv263.dc@gmail.com>
2020-03-19 00:25:35 +0530

    added more features and default files for index and 404 if not present in config

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,6 +3,9 @@ use server::Server;
 
 fn main() -> std::io::Result<()> {
     let server = Server::new(5);
-    server.start_at("0.0.0.0:8080", "config.txt").join().unwrap();
+    server
+        .start_at("0.0.0.0:8080", "config.txt")
+        .join()
+        .unwrap();
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
+use std::str::FromStr;
 extern crate chrono;
 use chrono::prelude::*;
 
@@ -50,19 +51,20 @@ impl Server {
             .append(true)
             .open("./ips.txt")
             .unwrap();
-    
+        let parser = Parse::new(config);
         let thread = thread::Builder::new()
             .name("server_thread".to_string())
             .spawn(move || loop {
-                let parser = Parse::new(config);
                 if self.is_dead() {
                     thread::sleep(Duration::from_millis(
                         (self.workers * 200).try_into().unwrap(),
                     ));
                     break;
                 }
+
                 match listener.accept() {
                     Ok((stream, addr)) => {
+                        let parser = parser.make_copy();
                         self.execute(move || {
                             parser.handle(stream, is_debug);
                         });
@@ -95,7 +97,9 @@ impl Drop for Server {
 
 struct Parse {
     index: String,
-    error_404: String
+    error_404: String,
+    has_index: bool,
+    has_error: bool,
 }
 
 impl Parse {
@@ -103,6 +107,8 @@ impl Parse {
         let config = File::open(filename).unwrap();
         let mut index = String::new();
         let mut error_404 = String::new();
+        let mut has_index = true;
+        let mut has_error = true;
         for line in BufReader::new(config).lines() {
             let line = line.unwrap();
             if line.starts_with("index:") {
@@ -111,11 +117,25 @@ impl Parse {
             } else if line.starts_with("404:") {
                 error_404 = String::from(line)
                     .split(' ').collect::<Vec<&str>>()[1].to_string();
+            } else if !line.starts_with("#") && line.len() > 0 {
+                println!("Garbage in config file: {}", line);
             }
         }
+
+        if index.is_empty() {
+            println!("No index file provided. Using dummy file.");
+            has_index = false;
+        }
+        if error_404.is_empty() {
+            println!("No 404 file provided. Using dummy file.");
+            has_error = false;
+        }
+
         Parse {
             index,
-            error_404
+            error_404,
+            has_index,
+            has_error,
         }
     }
 
@@ -138,21 +158,43 @@ impl Parse {
             let mut file_path = String::from(String::from_utf8(buffer.to_vec())
                 .unwrap().split(' ').collect::<Vec<&str>>()[1]);
             let filename = if file_path == "/" {
-                self.index.as_str()
-            } else {
+                if self.has_index {
+                    self.index.as_str()
+                } else {
+                    "dummy.html"
+                }
+            }  else {
                 file_path.remove(0);
                 file_path.as_str()
             };
             if is_debug {
                 println!("file_name: {}", filename);
             }
-            let contents = fs::read_to_string(filename).unwrap_or_else(|_|{
-                fs::read_to_string(self.error_404.as_str()).unwrap()
-            });
-            let content_type = check_ext(&String::from(filename));
+            let contents: String;
+            let mut response_type: &str;
+            let mut content_type = check_ext(&String::from(filename));
+            if !self.has_index && file_path == "/" && !self.has_error {
+                contents =
+                    "<!DOCTYPE html><html><body>No index file</body></html>"
+                    .to_string();
+                response_type = "200 OK";
+            } else {
+                response_type = "200 OK";
+                contents = fs::read_to_string(filename).unwrap_or_else(|_|{
+                    response_type = "404 NOT FOUND";
+                    content_type = "html".to_string();
+                    if self.has_error {
+                        fs::read_to_string(self.error_404.as_str())
+                        .unwrap()
+                    } else {
+                        "<!DOCTYPE html><html><body>No 404 file</body></html>"
+                        .to_string()
+                    }
+                });
+            };
             let status_line = format!(
-                "HTTP/1.1 \r\nContent-Type: text/{}\r\n\r\n",
-                content_type
+                "HTTP/1.1 {}\r\nContent-Type: text/{}\r\n\r\n",
+                response_type, content_type
             );
             let response = format!("{}{}", status_line, contents);
             if is_debug {
@@ -163,6 +205,20 @@ impl Parse {
         } else if buffer.starts_with(put) {
         }
     }
+
+    pub fn make_copy(&self) -> Parse {
+        let index = String::from_str(self.index.as_str()).unwrap();
+        let error_404 = String::from_str(self.error_404.as_str()).unwrap();
+        let has_index = self.has_index;
+        let has_error = self.has_error;
+
+        Parse {
+            index,
+            error_404,
+            has_index,
+            has_error,
+        }
+    }
 }
 
 fn check_ext(filename: &String) -> String {
@@ -171,6 +227,6 @@ fn check_ext(filename: &String) -> String {
     } else if filename.ends_with(".html") {
         "html".to_string()
     } else {
-        "html".to_string()
+        "plain".to_string()
     }
 }
